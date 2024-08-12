8cb44ec71c71f12dd892c4f4c0ffb1b607542dff
Umesh9045 <umeshsonawane9045@gmail.com>
2024-08-12 15:43:52 +0530

    Add project files.

---
+++ LoginFormSession/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginFormSession.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LoginFormSession/Models/UserTbl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginFormSession.Models;
+
+public partial class UserTbl
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Gender { get; set; }
+
+    [Required]
+    public int? Age { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
